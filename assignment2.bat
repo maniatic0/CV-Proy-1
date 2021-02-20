d270acb2310042abf931c48ea150224c49f92801
Christian Oliveros <christianol_01@hotmail.com>
2021-02-20 17:21:40 +0100

    Assigment 2 Calibration

--- assignment2.bat
+++ assignment2.bat
@@ -1,6 +1,6 @@
-SET CV_1=.\x64\Release\CameraCalibration.exe
+SET "CV_1=.\x64\Release\CameraCalibration.exe"
 SET CAM_NUMBER=4
 
-for /l %i in (1, 1, %CAM_NUMBER%) do (
-    CALL %CV_1% .\tests\cam%i%\calibration.xml
+for /l %%i in ( 1, 1, %CAM_NUMBER% ) do (
+    CALL %CV_1% .\tests\cam%%i\calibration.xml
 )
